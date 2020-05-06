c59fa0f3b76f8c87fe358e1c24d27d8522b90d78
Cadu Castanha <caducastanha@gmail.com>
2020-05-05 22:04:32 -0300

    Listagem de planos

--- src/app/controllers/PlanController.js
+++ src/app/controllers/PlanController.js
@@ -19,6 +19,14 @@ class PlanController {
 
     return res.json(plan);
   }
+
+  async index(req, res) {
+    const plans = await Plan.findAll({
+      attributes: ['id', 'title', 'duration', 'price'],
+    });
+
+    return res.json(plans);
+  }
 }
 
 export default new PlanController();
--- src/routes.js
+++ src/routes.js
@@ -15,5 +15,6 @@ routes.post('/students', StudentController.store);
 routes.put('/students', studentMiddleware, StudentController.update);
 
 routes.post('/plans', PlanController.store);
+routes.get('/plans', PlanController.index);
 
 export default routes;
